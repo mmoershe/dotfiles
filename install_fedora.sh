f66c218580db21ddfe46d631f5cbbb2a64c42da1
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-04-07 23:17:22 +0200

    adding some packages (flatpaks, dnf packages and vscodium)

--- install_fedora.sh
+++ install_fedora.sh
@@ -68,6 +68,11 @@ install_specials() {
         git clone https://github.com/mmoershe/config.nvim "$HOME/.config/nvim"
     fi
 
+    # Installing VSCodium
+    sudo rpmkeys --import https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg
+    printf "[gitlab.com_paulcarroty_vscodium_repo]\nname=download.vscodium.com\nbaseurl=https://download.vscodium.com/rpms/\nenabled=1\ngpgcheck=1\nrepo_gpgcheck=1\ngpgkey=https://gitlab.com/paulcarroty/vscodium-deb-rpm-repo/-/raw/master/pub.gpg\nmetadata_expire=1h\n" | sudo tee -a /etc/yum.repos.d/vscodium.repo
+    sudo dnf install codium -y
+
     echo "[+] Special packages installed."
 }
 
