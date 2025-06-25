b4227ffd15cf88452faed4971fd106d1070b46a6
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-06-25 17:50:47 +0200

    created generic flatpak script

---
+++ MISC/generic/install_flatpaks.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+set -e # Exit on error
+
+install_flatpaks() {
+    echo
+    echo "[ ] Installing Flatpaks..."
+
+    CURRENT_DIRECTORY="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+    FLATPAK_FILE="${CURRENT_DIRECTORY}/flatpaks.txt"
+    FLATPAKS=($(grep -vE '^\s*#|^\s*$' "$FLATPAK_FILE"))
+
+    if ! command -v flatpak >/dev/null 2>&1; then
+        echo "    [!] Error: Flatpak is not installed. Exiting."
+        exit 1
+    fi
+
+    for pkg in "${FLATPAKS[@]}"; do
+        flatpak install -y --noninteractive flathub "$pkg"
+        echo "    [✓] Installed "$pkg""
+    done
+
+    echo "    [ ] Updating Flatpak..."
+    sudo flatpak update -y
+    echo "    [✓] Updated Flatpak."
+
+    echo
+
+    echo "[+] Flatpak apps installed."
+    echo
+}
+
+if [[ "${BASH_SOURCE[0]}" == "${0}" ]]; then
+    install_flatpaks
+fi
