c6f8a16d803a5ac8bddc4ada212f49aa3ee4215d
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-10-23 21:53:46 +0200

    added inputconf and bindings.conf + deploy adjustments

--- platforms/omarchy/deploy_omarchy.sh
+++ platforms/omarchy/deploy_omarchy.sh
@@ -8,4 +8,8 @@ rm "$TARGET_DIR/.bashrc"
 stow --dir="$DOTFILES_DIR" --target="$TARGET_DIR" bash
 
 rm  "$TARGET_DIR/.config/hypr/monitors.conf"
+rm  "$TARGET_DIR/.config/hypr/input.conf"
+rm  "$TARGET_DIR/.config/hypr/bindings.conf"
 stow --dir="$DOTFILES_DIR" --target="$TARGET_DIR" hypr
+
+hyprctl reload
