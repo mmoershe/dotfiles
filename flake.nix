1f6234f917464c938b5ddefd7fad2aca2a961b14
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-11 18:13:26 +0100

    added proper homeConfigurations

--- flake.nix
+++ flake.nix
@@ -19,13 +19,13 @@
 	inherit system;
 	modules = [
 	  ./configuration.nix
-	  home-manager.nixosModules.home-manager
-	  {
-	    home-manager.useGlobalPkgs = true;
-	    home-manager.useUserPackages = true;
-	    home-manager.users.rellana = import ./home.nix;
-	  }
 	];
+      };
+    };
+    homeConfigurations = {
+      rellana = home-manager.lib.homeManagerConfiguration {
+        inherit pkgs;
+	modules = [ ./home.nix ];
       };
     };
   };
