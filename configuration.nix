5b56ad2a9cb7ffad54f9962a5de4cc8e6ae5bbe9
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-12 00:21:20 +0100

    added basic packages

--- configuration.nix
+++ configuration.nix
@@ -102,6 +102,8 @@
   # List packages installed in system profile. To search, run:
   # $ nix search wget
   environment.systemPackages = with pkgs; [
+    gcc
+    make
     vim
     wget
     neovim
