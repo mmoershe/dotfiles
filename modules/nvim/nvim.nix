b251f5f3de1b9e0ba4e05fff2320b22552413099
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-12 00:09:13 +0100

    using submodule for nvim

--- modules/nvim/nvim.nix
+++ modules/nvim/nvim.nix
@@ -1,12 +1,7 @@
 { config, pkgs, lib, ... }:
 
-let
-  nvimConfig = pkgs.fetchgit {
-    url = "https://github.com/mmoershe/config.nvim.git";
-    leaveUnspecifiedSha256 = true;
-  };
-in
 {
-  home.file.".config/nvim".source = nvimConfig;
+  home.file.".config/nvim".source = ./config.nvim;
 }
+
 
