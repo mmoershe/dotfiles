f4be1d886c63a876bb5b898207422372aa6324f1
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-11 23:53:00 +0100

    using fetchGit

--- modules/nvim/nvim.nix
+++ modules/nvim/nvim.nix
@@ -1,9 +1,9 @@
 { config, pkgs, lib, ... }:
 
 let
-  nvimConfig = pkgs.runCommand "nvim-config" {} ''
-    git clone https://github.com/mmoershe/config.nvim.git $out
-  '';
+  nvimConfig = builtins.fetchGit {
+    url = "https://github.com/mmoershe/config.nvim.git";
+  };
 in
 {
   home.file.".config/nvim".source = nvimConfig;
