a3944903a6befc1ea788ec9f83146b72e661f6e0
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-11 17:39:24 +0100

    trying to add home manager in flake.nix

--- flake.nix
+++ flake.nix
@@ -3,17 +3,29 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.05";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
   };
 
-  outputs = {self, nixpkgs, ... }:
+  outputs = {self, nixpkgs, home-manager, ... }:
     let 
       lib = nixpkgs.lib;
+      system = "x86_64-linux";
+      pkgs = nixpkgs.legacyPackages.${system};
     in {
     nixosConfigurations = {
       rellana = lib.nixosSystem {
-        system = "x86_64-linux";
-	modules = [ ./configuration.nix ];
+	inherit system;
+	modules = [
+	  ./configuration.nix
+	  home-manager.nixosModules.home-manager
+	  {
+	    home-manager.useGlobalPkgs = true;
+	    home-manager.useUserPackages = true;
+	    home-manager.users.rellana = import ./home.nix;
+	  }
+	];
       };
     };
-    };
+  };
 }
