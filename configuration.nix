e5d5130d400257f5b8b282da119d3ee854f6c66e
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-11 23:23:30 +0100

    removing gnome

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,6 @@
 
   # Enable the GNOME Desktop Environment.
   services.xserver.displayManager.gdm.enable = true;
-  services.xserver.desktopManager.gnome.enable = true;
 
   # Hyprland stuff
   programs.hyprland.enable = true;
