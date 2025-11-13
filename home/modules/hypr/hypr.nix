ce65ff2e5a19f1dbfa8f802d380792458b8ba5a4
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-13 03:48:50 +0100

    adding hyprland wallpaper

--- home/modules/hypr/hypr.nix
+++ home/modules/hypr/hypr.nix
@@ -9,6 +9,7 @@ in
         "${hyprConfigDir}/monitor.conf".source  = ./monitor.conf;
         "${hyprConfigDir}/bindings.conf".source  = ./bindings.conf;
         "${hyprConfigDir}/autostart.conf".source  = ./autostart.conf;
+        "${hyprConfigDir}/wallpaper_1".source  = ./wallpaper_1.jpeg;
     };
 }
 
