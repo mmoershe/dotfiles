b507ce92b93e799513a09d91d90aed12cc0038c0
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-13 01:02:20 +0100

    added hyprland waybar autostart

--- home/modules/hypr/hypr.nix
+++ home/modules/hypr/hypr.nix
@@ -7,5 +7,6 @@ in
   home.file."${hyprConfigDir}/hyprland.conf".source = ./hyprland.conf;
   home.file."${hyprConfigDir}/monitor.conf".source  = ./monitor.conf;
   home.file."${hyprConfigDir}/bindings.conf".source  = ./bindings.conf;
+  home.file."${hyprConfigDir}/autostart.conf".source  = ./autostart.conf;
 }
 
