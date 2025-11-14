4145a4fea836eb887ee5bf8565e3073367e7f34c
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-14 03:37:23 +0100

    changing animations.conf to looknfeel.conf

--- home/modules/hypr/hypr.nix
+++ home/modules/hypr/hypr.nix
@@ -9,7 +9,7 @@ in
         "${hyprConfigDir}/monitor.conf".source  = ./monitor.conf;
         "${hyprConfigDir}/bindings.conf".source  = ./bindings.conf;
         "${hyprConfigDir}/autostart.conf".source  = ./autostart.conf;
-        "${hyprConfigDir}/animations.conf".source  = ./animations.conf;
+        "${hyprConfigDir}/looknfeel.conf".source  = ./looknfeel.conf;
         "${hyprConfigDir}/wallpaper_1".source  = ./wallpaper_1.jpeg;
     };
 }
