6810c769b2854ec12465442805690cd493bfa437
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-16 04:55:20 +0100

    added working wallpaper with hyprpaper

--- home/modules/hypr/hypr.nix
+++ home/modules/hypr/hypr.nix
@@ -4,13 +4,18 @@ let
     hyprConfigDir = ".config/hypr";
 in
 {
+    home.packages = with pkgs; [
+        hyprpaper
+    ];
+
     home.file = {
         "${hyprConfigDir}/hyprland.conf".source = ./hyprland.conf;
         "${hyprConfigDir}/monitor.conf".source  = ./monitor.conf;
         "${hyprConfigDir}/bindings.conf".source  = ./bindings.conf;
         "${hyprConfigDir}/autostart.conf".source  = ./autostart.conf;
         "${hyprConfigDir}/looknfeel.conf".source  = ./looknfeel.conf;
-        "${hyprConfigDir}/wallpaper_1".source  = ./wallpaper_1.jpeg;
+        "${hyprConfigDir}/hyprpaper.conf".source = ./hyprpaper.conf;
+        "Pictures/wallpapers/wallpaper_1.jpeg".source  = ./wallpaper_1.jpeg;
     };
 }
 
