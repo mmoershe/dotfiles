c46b9c7c45b905ac4d467988d6a521e0acf2c90a
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-17 02:43:40 +0100

    added display server script

---
+++ scripts/check_display_server.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -e # Exit on error
+
+if [ "$XDG_SESSION_TYPE" = "wayland" ]; then
+    echo "Your system is using Wayland."
+elif [ "$XDG_SESSION_TYPE" = "x11" ]; then
+    echo "Your system is using X11."
+else
+    echo "Could not determine display server (X11 or Wayland)."
+fi
