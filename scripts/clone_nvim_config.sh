ef1c3f185939bb3ccf3ffbc48882cfd8782c1fc5
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-20 23:35:52 +0100

    updated Shebang line

--- scripts/clone_nvim_config.sh
+++ scripts/clone_nvim_config.sh
@@ -1,9 +1,10 @@
-#!/bin/bash
+#!/usr/bin/env bash
+
 set -e # Exit on error
 
 if [ -d "$HOME/.config/nvim" ]; then
-  echo "Neovim-Config already exists."
-  # maybe update the config (pull)
+    echo "Neovim-Config already exists."
+    # maybe update the config (pull)
 else
-  git clone https://github.com/mmoershe/config.nvim "$HOME/.config/nvim"
+    git clone https://github.com/mmoershe/config.nvim "$HOME/.config/nvim"
 fi
