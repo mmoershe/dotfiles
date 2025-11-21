781cce25a750fad5cb261d33a2e286b8e8bbefe5
mmoershe <92526921+mmoershe@users.noreply.github.com>
2025-11-21 12:14:32 +0100

    added show-branding script

--- system/scripts.nix
+++ system/scripts.nix
@@ -4,6 +4,7 @@ let
     my-scripts = with pkgs; [
         (writeShellScriptBin "clone-nvim"      (builtins.readFile ./scripts/clone_nvim_config.sh))
         (writeShellScriptBin "check-display-server" (builtins.readFile ./scripts/check_display_server.sh))
+        (writeShellScriptBin "show-branding" (builtins.readFile ./scripts/show-branding.sh))
     ];
 
 in
---
+++ system/scripts/show-branding.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+set -e
+
+clear
+echo -e "\033[35m"
+cat ~/.config/branding.txt
+echo -e "\033[0m"
+echo
