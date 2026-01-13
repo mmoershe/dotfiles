342b5810de2740b1dd738ef55209872dc10592e0
mmoershe <92526921+mmoershe@users.noreply.github.com>
2026-01-13 14:53:38 +0100

    change some script names

--- system/scripts.nix
+++ system/scripts.nix
@@ -2,8 +2,8 @@
 
 let
     my-scripts = with pkgs; [
-        (writeShellScriptBin "rellanix-nvim"      (builtins.readFile ./scripts/clone_nvim_config.sh))
-        (writeShellScriptBin "rellanix-display-server" (builtins.readFile ./scripts/check_display_server.sh))
+        (writeShellScriptBin "rellanix-nvim"      (builtins.readFile ./scripts/clone-nvim-config.sh))
+        (writeShellScriptBin "rellanix-display-server" (builtins.readFile ./scripts/check-display-server.sh))
         (writeShellScriptBin "rellanix-branding" (builtins.readFile ./scripts/show-branding.sh))
         (writeShellScriptBin "rellanix-screensaver" (builtins.readFile ./scripts/screensaver.sh))
         (writeShellScriptBin "rellanix-launch-screensaver" (builtins.readFile ./scripts/launch-screensaver.sh))
