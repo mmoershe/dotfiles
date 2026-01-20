43fcb3b12017a83c7f868fff6f95f109c37ba369
mmoershe <92526921+mmoershe@users.noreply.github.com>
2026-01-20 21:57:57 +0100

    fixed link in clone nvim config

--- system/scripts/clone-nvim-config.sh
+++ system/scripts/clone-nvim-config.sh
@@ -3,7 +3,7 @@
 set -e # Exit on error
 
 NVIM_CONFIG_DIR="$HOME/.config/nvim"
-NVIM_CONFIG_REPO="https://github.com/mmoershe/nvim.config"
+NVIM_CONFIG_REPO="https://github.com/mmoershe/config.nvim"
 
 if [ -d "$HOME/.config/nvim" ]; then
     echo "Neovim-Config already exists. Pulling changes..."
