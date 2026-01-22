4ac6b8f113727bd2aa6f2a891923f10f84178895
mmoershe <92526921+mmoershe@users.noreply.github.com>
2026-01-22 17:43:41 +0100

    NVF: highlight on yank

--- home/modules/nvim/modules/autocmd.nix
+++ home/modules/nvim/modules/autocmd.nix
@@ -1,5 +1,19 @@
+{lib, ...}:
+let
+  inherit (lib.generators) mkLuaInline;
+in
 {
   programs.nvf.settings.vim = {
-    autocmds = [];
+    autocmds = [
+      {
+        event = ["TextYankPost"];
+        pattern = ["*"];
+        callback = mkLuaInline ''
+          function()
+            vim.highlight.on_yank()
+          end
+        '';
+      }
+    ];
   };
 }
