8ad03058ae280722ffb749af7019326ee837c978
mmoershe <92526921+mmoershe@users.noreply.github.com>
2026-04-22 03:14:29 +0200

    added some more packages

--- scripts/install_packages.sh
+++ scripts/install_packages.sh
@@ -91,6 +91,7 @@ install_packages() {
         install_pacman_packages_from_file $PACKAGES_DIR/desktop.txt
         install_pacman_packages_from_file $PACKAGES_DIR/games.txt
         install_aur_packages_from_file $PACKAGES_AUR_DIR/code.txt
+        install_aur_packages_from_file $PACKAGES_AUR_DIR/desktop.txt
         install_aur_packages_from_file $PACKAGES_AUR_DIR/games.txt
     fi
 
@@ -100,6 +101,7 @@ install_packages() {
         install_pacman_packages_from_file $PACKAGES_DIR/hyprland.txt
         install_pacman_packages_from_file $PACKAGES_DIR/desktop.txt
         install_aur_packages_from_file $PACKAGES_AUR_DIR/code.txt
+        install_aur_packages_from_file $PACKAGES_AUR_DIR/desktop.txt
     fi
 
     if [[ $PROFILE == "thiollier" ]]; then
@@ -108,6 +110,7 @@ install_packages() {
         install_pacman_packages_from_file $PACKAGES_DIR/hyprland.txt
         install_pacman_packages_from_file $PACKAGES_DIR/desktop.txt
         install_aur_packages_from_file $PACKAGES_AUR_DIR/code.txt
+        install_aur_packages_from_file $PACKAGES_AUR_DIR/desktop.txt
         install_aur_packages_from_file $PACKAGES_AUR_DIR/thiollier.txt
     fi
 
